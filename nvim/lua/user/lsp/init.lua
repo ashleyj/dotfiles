2675495cc45196c7f5fa46819a27fa9ff6e9452c
Ashley Williams <ashleyw@beigetech.com.au>
2025-02-16 09:05:19 +1000

    fixed erroring dartls (not supporeted by mason-lspconfig

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -6,6 +6,8 @@ end
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
+require'lspconfig'.dartls.setup{}
+
 
 --local cfg = {}  -- add your config here
 --require "lsp_signature".setup(cfg)
--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -9,8 +9,7 @@ local servers = {
 	"yamlls",
 	"omnisharp",
 	"intelephense",
-	"angularls",
-	"dartls"
+	"angularls"
 }
 
 local settings = {
@@ -31,6 +30,7 @@ require("mason-lspconfig").setup({
 	ensure_installed = servers,
 	automatic_installation = true,
 })
+
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
