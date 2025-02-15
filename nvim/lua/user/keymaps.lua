6a9de4aa96549175451ac7be900c70db12982e4a
Ashley Williams <ashleyw@beigetech.com.au>
2025-02-15 16:48:58 +1000

    removed commented out keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -106,22 +106,6 @@ keymap("n", "<leader>di", "<Cmd>lua require'dap'.step_into()<CR>",opts)
 keymap("n", "<leader>do", "<Cmd>lua require'dap'.step_out()<CR>",opts)
 
 
--- VimSpector --
---nnoremap <Leader>dt :call vimspector#ToggleBreakpoint()<CR>
---nnoremap <Leader>dT :call vimspector#ClearBreakpoints()<CR>
-
-
--- nnoremap <silent> <F5> <Cmd>lua require'dap'.continue()<CR>
---nnoremap <silent> <F10> <Cmd>lua require'dap'.step_over()<CR>
---nnoremap <silent> <F11> <Cmd>lua require'dap'.step_into()<CR>
---nnoremap <silent> <F12> <Cmd>lua require'dap'.step_out()<CR>
---nnoremap <silent> <Leader>b <Cmd>lua require'dap'.toggle_breakpoint()<CR>
---nnoremap <silent> <Leader>B <Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
---nnoremap <silent> <Leader>lp <Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
---nnoremap <silent> <Leader>dr <Cmd>lua require'dap'.repl.open()<CR>
---nnoremap <silent> <Leader>dl <Cmd>lua require'dap'.run_last()<CR>
-
-
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
