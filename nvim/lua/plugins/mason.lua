96bf5856e3c8ca0140e2ff71e471cadb9fd21b60
Ashley Williams <ashleyw@beigetech.com.au>
2025-12-11 18:54:42 +1000

    added jdtls config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,15 +1,123 @@
+-- Shared on_attach function for all LSP servers
+local function on_attach(client, bufnr)
+  local opts = { noremap = true, silent = true }
+  local keymap = vim.api.nvim_buf_set_keymap
+  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  keymap(bufnr, "n", "<leader>r", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+  keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+  keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+  keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+  keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+  keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+  keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+  keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+
+  if client.name == "omnisharp" then
+    keymap(bufnr, "n", "gd", "<cmd>lua require('omnisharp_extended').lsp_definition()<cr>", opts)
+    keymap(bufnr, "n", "gr", "<cmd>lua require('omnisharp_extended').lsp_references()<cr>", opts)
+    keymap(bufnr, "n", "gi", "<cmd>lua require('omnisharp_extended').lsp_implementations()<cr>", opts)
+    keymap(bufnr, "n", '<leader>D', "<cmd>lua require('omnisharp_extended').lsp_type_definition()<cr>", opts)
+  end
+
+  client.server_capabilities.documentHighlightProvider = false
+  client.capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  if client.server_capabilities.signatureHelpProvider then
+    require('lsp-overloads').setup(client, {
+      ui = {
+        border = "single",
+        height = nil,
+        width = nil,
+        wrap = true,
+        wrap_at = nil,
+        max_width = nil,
+        max_height = nil,
+        close_events = { "CursorMoved", "BufHidden", "InsertLeave" },
+        focusable = true,
+        focus = false,
+        offset_x = 0,
+        offset_y = 0,
+        floating_window_above_cur_line = false,
+        silent = true,
+        highlight = {
+          italic = true,
+          bold = true,
+          fg = "#ffffff",
+        }
+      },
+      keymaps = {
+        next_signature = "<C-j>",
+        previous_signature = "<C-k>",
+        next_parameter = "<C-l>",
+        previous_parameter = "<C-h>",
+        close_signature = "<c-q>"
+      },
+      display_automatically = true
+    })
+  end
+end
+
 return {
   "williamboman/mason.nvim",
+  event = "VeryLazy",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
     "ashleyj/lsp_signature.nvim",
     "Issafalcon/lsp-overloads.nvim",
-    "Hoffs/omnisharp-extended-lsp.nvim"
+    "Hoffs/omnisharp-extended-lsp.nvim",
+    {
+      "mfussenegger/nvim-jdtls",
+      ft = "java",
+      config = function()
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "java",
+          callback = function()
+            local jdtls = require('jdtls')
+
+            -- Find the root directory, preferring parent pom.xml or .git
+            local root_dir = vim.fs.root(0, {'.git', 'mvnw', 'gradlew'})
+            if not root_dir then
+              root_dir = vim.fs.root(0, {'pom.xml'})
+            end
+
+            local project_name = vim.fn.fnamemodify(root_dir or vim.fn.getcwd(), ':p:h:t')
+            local workspace_dir = vim.fn.stdpath('cache') .. '/jdtls/workspace/' .. project_name
+
+            -- Create capabilities
+            local cmp = require("cmp_nvim_lsp")
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
+            capabilities = cmp.default_capabilities(capabilities)
+
+            local config = {
+              cmd = {"/home/ash/src/jdtls/bin/jdtls", "-data", workspace_dir},
+              root_dir = root_dir,
+              settings = {
+                java = {}
+              },
+              init_options = {
+                bundles = {}
+              },
+              on_attach = on_attach,
+              capabilities = capabilities
+            }
+
+            jdtls.start_or_attach(config)
+          end
+        })
+      end
+    },
   },
 
   config = function()
     local servers = {
+      "jdtls",
       "lua_ls",
       "cssls",
       "html",
@@ -60,83 +168,6 @@ return {
       border = "rounded",
     })
 
-
-    -- re-usable on_attach function
-
-    local function on_attach(client, bufnr)
-      -- setup keymaps
-      local opts = { noremap = true, silent = true }
-      local keymap = vim.api.nvim_buf_set_keymap
-      keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-      keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-      keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-      keymap(bufnr, "n", "<leader>r", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-      keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-      keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
-      keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-      keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
-      keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-      keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
-      keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
-      keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-      keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-      keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-
-      if client == "omnisharp" then
-        keymap(bufnr, "n", "gd", "<cmd>lua require('omnisharp_extended').lsp_definition()<cr>", opts)
-        keymap(bufnr, "n", "gr", "<cmd>lua require('omnisharp_extended').lsp_references()<cr>", opts)
-        keymap(bufnr, "n", "gi", "<cmd>lua require('omnisharp_extended').lsp_implementations()<cr>", opts)
-        keymap(bufnr, "n", '<leader>D', "<cmd>lua require('omnisharp_extended').lsp_type_definition()<cr>", opts)
-      end
-
-
-      client.server_capabilities.documentHighlightProvider = false
-      --client.server_capabilities.semanticTokensProvider = nil
-
-
-
-      client.capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-      if client.server_capabilities.signatureHelpProvider then
-        require('lsp-overloads').setup(client, {
-          -- UI options are mostly the same as those passed to vim.lsp.util.open_floating_preview
-          ui = {
-            border = "single", -- The border to use for the signature popup window. Accepts same border values as |nvim_open_win()|.
-            height = nil,      -- Height of the signature popup window (nil allows dynamic sizing based on content of the help)
-            width = nil,       -- Width of the signature popup window (nil allows dynamic sizing based on content of the help)
-            wrap = true,       -- Wrap long lines
-            wrap_at = nil,     -- Character to wrap at for computing height when wrap enabled
-            max_width = nil,   -- Maximum signature popup width
-            max_height = nil,  -- Maximum signature popup height
-            -- Events that will close the signature popup window: use {"CursorMoved", "CursorMovedI", "InsertCharPre"} to hide the window when typing
-            close_events = { "CursorMoved", "BufHidden", "InsertLeave" },
-            focusable = true,                       -- Make the popup float focusable
-            focus = false,                          -- If focusable is also true, and this is set to true, navigating through overloads will focus into the popup window (probably not what you want)
-            offset_x = 0,                           -- Horizontal offset of the floating window relative to the cursor position
-            offset_y = 0,                           -- Vertical offset of the floating window relative to the cursor position
-            floating_window_above_cur_line = false, -- Attempt to float the popup above the cursor position
-            -- (note, if the height of the float would be greater than the space left above the cursor, it will default
-            -- to placing the float below the cursor. The max_height option allows for finer tuning of this)
-            silent = true, -- Prevents noisy notifications (make false to help debug why signature isn't working)
-            -- Highlight options is null by default, but this just shows an example of how it can be used to modify the LspSignatureActiveParameter highlight property
-            highlight = {
-              italic = true,
-              bold = true,
-              fg = "#ffffff",
-            }
-          },
-          keymaps = {
-            next_signature = "<C-j>",
-            previous_signature = "<C-k>",
-            next_parameter = "<C-l>",
-            previous_parameter = "<C-h>",
-            close_signature = "<c-q>"
-          },
-          display_automatically = true -- Uses trigger characters to automatically display the signature overloads when typing a method signature
-        })
-      end
-    end
-
     local settings = {
       ui = {
         border = "none",
@@ -179,6 +210,11 @@ return {
         }
 
         require 'lspconfig'.omnisharp.setup(opts)
+        goto continue
+      end
+
+      if server == "jdtls" then
+        -- Skip jdtls in the main loop, it's handled by autocmd below
         goto continue
       end
 
